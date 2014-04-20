eaa27395c539a4ac77067611e574d11fb9b73969
Philip Wellnitz <philipwellnitz@gmx.de>
2014-04-20 17:01:54 +0200

    A2 work

--- Aufgabe_2/Lebenslinien.cpp
+++ Aufgabe_2/Lebenslinien.cpp
@@ -3,12 +3,13 @@
 #include <list>
 #include <stack>
 #include <deque>
+#include <algorithm>
 
 std::vector<std::vector<int> > G; //The Graph
 int N; //Graphsize
 
 void readInput(){
-   scanf("%i",&N);
+  scanf("%i",&N);
   G.assign(N,std::vector<int>(N,0));
   
   //Read Graph stored in an Adjancy Matrix
@@ -21,7 +22,7 @@ void readInput(){
 //BEGIN lexicographic BFS
 
 std::vector<int> LexBFSOrder(){
-//   std::vector<int> ret(G.size(),-1);
+ //   std::vector<int> ret(G.size(),-1);
   
   std::list<int> L;
   for(int i = 0; i < (int)G.size(); ++i)
@@ -29,15 +30,15 @@ std::vector<int> LexBFSOrder(){
   std::deque<std::list<int>> classes;
   classes.push_back(L);
     
-//   int cnt = G.size() - 1;
+ //   int cnt = G.size() - 1;
   
   std::deque<int> ret;
   
   while(!classes.empty()){
-//     for(auto i : classes)
-//       for(auto j : i)
-// 	printf("%d ",j + 1);
-//     printf("\n");
+ //     for(auto i : classes)
+ //       for(auto j : i)
+ //  	printf("%d ",j + 1);
+ //      printf("\n");
     
     int ac = *(classes.begin()->begin());
     classes.begin()->pop_front();
@@ -46,7 +47,7 @@ std::vector<int> LexBFSOrder(){
     
     ret.push_front(ac);
     
-//     ret[ac] = cnt--;
+ //     ret[ac] = cnt--;
     std::deque<std::list<int>> new_classes;
     for(auto i : classes){
       std::list<int> tmp_in, tmp_out;
@@ -77,7 +78,7 @@ std::vector<int> LexBFSOrder(){
 
 
 
-//checks if ordering is a perfect elimination ordering
+ //checks if ordering is a perfect elimination ordering
 bool isChordal(std::vector<int> ordering){
   
   std::vector<std::vector<int> > rightNeighs(N, std::vector<int> ()); //Neghbours to the right
@@ -113,7 +114,7 @@ bool isChordal(std::vector<int> ordering){
     if(rightNeighs[x].size() <= 1)
       continue;
     
-    //naive implementation
+   // naive implementation
     for(int i = 1; i < rightNeighs[x].size(); ++i){
       bool existant = false;
       for(auto j : rightNeighs[rightNeighs[x][0]])
@@ -127,6 +128,77 @@ bool isChordal(std::vector<int> ordering){
   }
   
   return true;
+}
+
+bool equals(std::vector<int>::iterator a1,std::vector<int>::iterator a2, std::vector<int>::iterator b1, std::vector<int>::iterator b2) {
+  if(std::distance(a2,a1) != std::distance(b2,b1))
+    return false;
+  
+  for(a1 != a2 && b1 != b2)
+    if(*(a1++) != *(b1) ++)
+      return false;
+    return true;
+}
+
+std::vector<std::pair<std::vector<std::vector<int> >, int > > getCliqueTree(std::vector<int> ordering) {
+  std::vector<std::vector<int> > rightNeighs(N, std::vector<int> ()); //Neghbours to the right
+  std::vector<std::vector<int> > T(N,std::vector<int>());
+  
+  std::vector<bool> used(N, false);
+  for(int x = 0; x < N; ++x){
+    int acnode = ordering[x];
+    
+    used[acnode] = true;
+    
+    for(int i = 0; i < N; ++i)
+      if(G[i][acnode] && used[i]){
+	if(rightNeighs[i].empty())
+	  T[acnode].push_back(i);
+	rightNeighs[i].push_back(acnode);
+      }
+  }  
+  
+  std::stack<std::pair<int,bool> > s;
+  std::vector<int> cliquePar(N,-1);
+  std::vector<int> clique(N,-1);
+  int cCnt = -1;
+  for(int i = 0; i< N; ++i)
+    if(T[i].empty())
+      s.push(pair<int,bool>(i,true)); //push the tree roots
+  while(!s.empty()){
+    auto ac = s.top(); 
+    s.pop();
+    
+    if(!ac.second) {//foreach vertex except the roots
+      std::sort(++rightNeighs[ac.first].begin(),rightNeighs[ac.first].end());
+      auto cpy = {rightNeighs[rightNeighs[ac.first][0]].begin(), rightNeighs[rightNeighs[ac.first][0]].end()};
+      std::sort(cpy.begin(), cpy.end());
+      if(equals(cpy.begin(), cpy.end(), ++rightNeighs[ac.first].begin(),rightNeighs[ac.first].end() )) {
+	clique[ac.first] = ++cCnt;
+	cliquePar[cCnt] = clique[rightNeighs[ac.first][0]];
+      }
+      else
+	clique[x] = clique[rightNeighs[ac.first][0]];
+    }
+    
+    for(auto i : T[ac.first])
+      s.push(pair<int,bool>(i,false));
+  }
+  
+  //reconstruct the tree cN²
+  
+  std::vector<std::pair<std::vector<std::vector<int> >, int > > T(cCnt, std::pair<std::vector<std::vector<int> >,int>());
+  
+  for(int i = 0; i < cCnt; ++i){
+    T[i].first.assign(N, vector<int>);
+    for(int j = 0; j < N; ++j)
+      if(clique[j] == i)
+	for(int k = 0; k < N; ++k)
+	  if(k != j && clique[k] == i)
+	    T[i].first[j].push_back(k);
+    T[i].second = cliquePar[i];
+  }
+  return T;
 }
 
 //END
@@ -188,6 +260,9 @@ int main(){
     printf("The graph is chordal!\n");
   else{
     printf("The graph is not chordal.\n"); //Calculation stops here
+    
+    //If the graph is not chordal, it has to contain a hole
+    //Hole finding starts here therefore.
     
     return 0;
   }
