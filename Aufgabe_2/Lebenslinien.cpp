55f49f174a3917980d975d2d0efb57f7a549de2a
Philip Wellnitz <philipwellnitz@gmx.de>
2014-04-25 07:56:21 +0200

    a2 work

--- Aufgabe_2/Lebenslinien.cpp
+++ Aufgabe_2/Lebenslinien.cpp
@@ -4,6 +4,7 @@
 #include <stack>
 #include <deque>
 #include <algorithm>
+#include <iterator>
 #include <set>
 #include <queue>
 
@@ -11,7 +12,7 @@ std::vector<std::vector<int> > G; //The Graph
 int N; //Graphsize
 
 void readInput(){
-  std::printf("The graphsuze.\n");
+  std::printf("The graphsize.\n");
   scanf("%i",&N);
   G.assign(N,std::vector<int>(N,0));
   
@@ -65,8 +66,6 @@ std::vector<int> LexBFSOrder(){
 
 //BEGIN Chordality test
 
-
-
  //checks if ordering is a perfect elimination ordering
 bool isChordal(std::vector<int> ordering){
   
@@ -82,22 +81,6 @@ bool isChordal(std::vector<int> ordering){
       if(G[i][acnode] && used[i])
 	rightNeighs[i].push_back(acnode);
   }
-  
-//   printf("RN | RN(par)\n");
-//   
-//   for(int i = 0; i < N; ++i){
-//     printf("%d:",i);
-//     
-//     for(auto j: rightNeighs[i])
-//       printf(" %d",j);
-//     printf(" |");
-//     
-//     if(rightNeighs[i].size() > 1)
-//       for(auto j: rightNeighs[rightNeighs[i][0]])
-// 	printf(" %d",j);
-//     
-//     printf("\n");
-//   }
   
   for(int x = 0; x < N; ++x){
     if(rightNeighs[x].size() <= 1)
@@ -120,7 +103,7 @@ bool isChordal(std::vector<int> ordering){
 }
 
 std::vector<std::pair<std::vector<int>, int > > 
-getCliqueTree(std::vector<int> ordering) {
+getCliqueTree(std::vector<int> ordering, bool printCliques = true) {
   std::vector<std::vector<int> > rightNeighs(N, std::vector<int> ()); //Neghbours to the right
   std::vector<std::vector<int> > T(N,std::vector<int>());
   
@@ -179,11 +162,13 @@ getCliqueTree(std::vector<int> ordering) {
       num[i] = cnt++;
   for(int i = 0; i < N; ++i)
     if(cliques.count(i) && clique[i].count(i)){
-      printf("In clique %d:", i);
-      for(auto k : clique[i])
-	printf(" %d", k);
-      printf("\n");
       
+      if(printCliques){
+	std::printf("In clique %d:", i);
+	for(auto k : clique[i])
+	  std::printf(" %d", k);
+	std::printf("\n");
+      }
       int par = (rightNeighs[i].empty() ? -1 : num[rightNeighs[i][0]]);
       t.push_back(std::pair<std::vector<int>, int >({clique[i].begin(),clique[i].end()}, par));
     }
@@ -196,115 +181,79 @@ getCliqueTree(std::vector<int> ordering) {
 
 std::deque<std::vector<int>> L; //clique chain
 
-bool isIntervalGraph(std::vector<int> ordering){
-  auto T = getCliqueTree(ordering);
+bool isIntervalGraph(std::vector<int> ordering, bool printOut = true){
+  auto T = getCliqueTree(ordering, printOut);
   
-  fflush(stdout);
+  std::list<std::vector<int> > l;
+  for(size_t o = 0; o < T.size(); ++o)
+    l.push_back(T[o].first);
   
-  std::list<std::pair<std::vector<int>,int> > l;
-  std::list<std::pair<int,int> > treeEdges;
-  for(size_t o = 0; o < T.size(); ++o){
-    auto i = T[o];
-    l.push_back(std::pair<std::vector<int>,int>(i.first,o));
-    if(i.second >= 0)
-      treeEdges.push_back(std::pair<int,int>(o,i.second));
-  }
-  std::list<std::list<std::pair<std::vector<int>, int> > > classes;
+  std::list<std::list< std::vector<int> > > classes;
   classes.push_back(l);
     
-  std::stack<int> pivots;
-  
   std::vector<bool> used(N, false);
   
+  L.clear();
+  
   while(!classes.empty()){
-    if(classes.rbegin()->size() == 1) {  //Skip classes containing only 1 element
-      L.push_front(classes.rbegin()->rbegin()->first);
-      classes.pop_back();
-      continue;
-    }
-    
-    std::set<int> C;
-    while(!pivots.empty() && used[pivots.top()])
-      pivots.pop();
-    
-    if(pivots.empty()){
-      while(classes.rbegin()->empty())
-	classes.pop_back();
-      auto Xc = *(classes.rbegin()->rbegin());
-      C.insert(Xc.second);
-      
-      
-      L.push_front(Xc.first);
-      classes.rbegin()->pop_back();
-      if(classes.rbegin()->empty())
-	classes.pop_back();
-      
-    } else {      
-      int x = pivots.top(); pivots.pop();
-      used[x] = true;
-      
-      auto Xa = classes.begin(), Xb = classes.begin();
-      bool Xaset = false;
-      
-      for(auto i = classes.begin(); i != classes.end(); ++i)
-	for(auto j : *i) {
-	  bool contains = false;
-	  for(auto k: j.first)
-	    if(k == x){
-	      contains = true;
-	      break;
-	    }
-	  if(contains)
-	    C.insert(j.second);
-	  
-	  Xb = i;
-	  if(!Xaset){
-	    Xa = i;
-	    Xaset = true;
-	  }
+  
+    if(printOut){
+      for(auto i = classes.begin(); i != classes.end(); ++i){
+	if(i != classes.begin())
+	  printf(" |");
+	for(auto j = i->begin(); j != i->end(); ++j){
+	  if(j != i->begin())
+	    printf(",");
+	  for(auto k : *j)
+	    printf(" %d",k);
 	}
-	
-      //Partion Xa und Xb
-      std::list<std::pair<std::vector<int>, int>> tmp_inA, tmp_outA, tmp_inB, tmp_outB;
-      
-      for(auto i : (*Xa))
-	if(C.count(i.second))
-	  tmp_inA.push_back(i);
-	else
-	  tmp_outA.push_back(i);
-      for(auto i : (*Xb))
-	if(C.count(i.second))
-	  tmp_inB.push_back(i);
-	else
-	  tmp_outB.push_back(i);
-      
-      (*Xa) = tmp_outA;
-      classes.insert(Xa, tmp_inA);
-      
-      (*Xb) = tmp_inB;
-      classes.insert(Xb, tmp_outB);
-      
-    }
-     
-    //Update Pivots
-    for(auto i = treeEdges.begin(); i != treeEdges.end(); ++i){
-      if(C.count(i->first) != C.count(i->second)){
-	std::set<int> Ci;
-	for(auto j : T[i->first].first)
-	  Ci.insert(j);
-	for(auto j : T[i->second].first)
-	  if(Ci.count(j))
-	    pivots.push(j);
-	  
-	treeEdges.erase(i);	
-	--i;
       }
+      printf("\n");
     }
-  }
     
+    
+    auto LST = *(classes.rbegin()->rbegin());
+    L.push_front(LST);
+    classes.rbegin()->pop_back();
+    
+    
+    std::sort(LST.begin(),LST.end());
+    
+    std::list< std::vector<int> > tmp_in, tmp_out;
+    std::vector< int >tmp;
+    for(auto I = classes.rbegin()->begin(); I != classes.rbegin()->end(); ++I){
+      tmp.clear();
+      
+      std::sort(I->begin(),I->end());
+      std::set_intersection(LST.begin(), LST.end(),
+			I->begin(), I->end(),
+			std::back_inserter(tmp));
+            
+      if(tmp.empty())
+	tmp_out.push_back(*I);
+      else
+	tmp_in.push_back(*I);
+      
+    }
+    classes.pop_back();
+    if(!tmp_out.empty())
+      classes.push_back(tmp_out);
+    if(!tmp_in.empty())
+      classes.push_back(tmp_in);
+  } 
+  
   std::vector<bool> alive(N,false), ended (N,false);
   
+  if(printOut)
+    printf("END\n");
   for(auto i : L){
+
+    if(printOut) {
+      for(auto j : i)
+	printf(" %d",j);
+      printf("\n");
+    }
+    
     std::vector<bool> seen (N, false);
     for(auto j : i)
       seen[j] = true;
@@ -324,20 +273,54 @@ bool isIntervalGraph(std::vector<int> ordering){
 
 //END
 
+//determines smallest subgraph that is not an interval graph
+std::vector<int> smallestFailingSubgraph(){
+  auto G2 = G;
+  int N2 = N;
+  for(N = 4; N <= N2; ++N){ //try all size i subsets of the nodes
+    std::vector<bool> btm(N2,false);
+    for(int i = N2-1; i >= N2 - N; --i)
+      btm[i] = true;
+    do {
+      G.assign(N,std::vector<int>(N,0));
+      int i = 0, j = 0;
+      
+      for(int k = 0; k < N2; ++k)
+	if(btm[k]){
+	  j = 0;
+	  for(int l = 0; l < N2; ++l)
+	    if(btm[l])
+	      G[i][j++] = G2[k][l];
+	  ++i;
+	}
+      auto LO = LexBFSOrder();
+      if(!isChordal(LO) || !isIntervalGraph(LO, false)) { //Found smallest subgraph, that is not chordal
+	
+	std::vector<int> ret;
+	
+	for(int k = 0; k < N2; ++k)
+	  if(btm[k])
+	    ret.push_back(k);
+	return ret;
+      }
+      
+    }while(std::next_permutation(btm.begin(),btm.end()));
+  }
+  
+  return std::vector<int>();
+}
+
 int main(){
   readInput();
   auto LO = LexBFSOrder(); 
   if(isChordal(LO))
-    printf("The graph is chordal!\n");
+    std::printf("The graph is chordal!\n");
   else{
-    printf("The graph is not chordal.\n"); //Calculation stops here
-    
-    auto G2 = G;
-    int N2 = N;
-    for(N = 4; N <= N2; ++N){ //try all size i subsets of the nodes
-      vector<bool> ac;
-      
-    }
+    std::printf("The graph is not chordal.\n"); //Calculation stops here
+   
+    std::printf("Smallest subgraph that is not chordal:\n");
+    for(auto i : smallestFailingSubgraph())
+      std::printf("Node %d\n",i);
     
     return 0;
   }
@@ -354,13 +337,16 @@ int main(){
       }
     }
     
-    printf("The graph is an interval graph:\n");
+    std::printf("The graph is an interval graph:\n");
     for(int i = 0; i < N; ++i)
-      printf("Node %d: 01.01.%4d - 01.01.%4d\n", i, 1900 + 2 * start[i], 1901 + 2 * start[i]);
+      std::printf("Node %d: 25.09.%4d - 16.10.%4d\n", i, 1900 + 2 * start[i], 1901 + 2 * end[i]);
   }
   else {
-    printf("The graph is not an interval graph.\n");
+    std::printf("The graph is not an interval graph.\n");
     
+    std::printf("Smallest subgraph that is not an interval graph:\n");
+    for(auto i : smallestFailingSubgraph())
+      std::printf("Node %d\n",i);
     
     return 0;
   }
